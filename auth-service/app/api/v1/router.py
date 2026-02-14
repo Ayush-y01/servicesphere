018df17cace11455b4066da18d165cc6fb5c5c64
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-02-14 20:31:12 +0530

    create access token based login security

---
+++ auth-service/app/api/v1/router.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from app.api.v1.routes import auth
+
+api_router = APIRouter(prefix="/api/v1")
+
+api_router.include_router(
+    auth.router,
+    prefix="/auth",
+    tags=["Auth"]
+)
--- auth-service/app/api/v1/routes/auth.py
+++ auth-service/app/api/v1/routes/auth.py
@@ -1,12 +1,33 @@
 from fastapi import APIRouter,Depends
 from app.schemas.auth import LoginRequest
 from app.services.auth_service import AuthService
+from sqlalchemy.orm import session
+
+from app.dependencies.db import get_db
+from app.schemas.auth import LoginRequest
+from app.services.auth_service import AuthService
+
+from app.dependencies.auth import get_current_user
 
 router = APIRouter()
+
+@router.post("/create_user")
+async def create_user(
+    data:LoginRequest,
+    db: session = Depends(get_db),
+    service: AuthService = Depends()
+):
+    user = await service.create_user(data.email, data.password, db)
+    return {"id":user.id, "email":user.email}
 
 @router.post("/login")
 async def login(
     data: LoginRequest,
+    db:session = Depends(get_db),
     service: AuthService = Depends()
 ):
-    return await service.login(data)
+    return await service.login(data,db)
+
+@router.get("/me")
+async def me(user_id: str = Depends(get_current_user)):
+    return {"user_id":user_id}
