d4ff74ce4e0eb0c2581bbc6ff30c4bb559f7f248
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-02-23 19:37:51 +0530

    refactor authentication logic to verify token via external service

--- user-service/app/core/security.py
+++ user-service/app/core/security.py
@@ -1,3 +1,4 @@
+import requests
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
@@ -5,27 +6,30 @@ from app.core.config import settings
 
 security = HTTPBearer()
 
+AUTH_VERIFY_URL = "http://auth-service:8001/auth/verify-token"
+
 def get_current_user(
-        credentials: HTTPAuthorizationCredentials = Depends(security)
+    credentials: HTTPAuthorizationCredentials = Depends(security)
 ):
     token = credentials.credentials
 
-    try:
-        payload = jwt.decode(
-            token,
-            settings.JWT_SECRET,
-            algorithms=[settings.jwt_ALGORITHM]
-        )
-        return payload
-    except JWTError:
+    response = requests.get(
+        AUTH_VERIFY_URL,
+        headers={"Authorization": f"Bearer {token}"}
+    )
+
+    if response.status_code != 200:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token"
+            detail="Invalid or expired token"
         )
-    
+
+    return response.json()
+
+
 def require_roles(allowed_roles: list[str]):
     def role_checker(current_user = Depends(get_current_user)):
-        if current_user.role not in allowed_roles:
+        if current_user["role"] not in allowed_roles:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="You Do Not have permission"
